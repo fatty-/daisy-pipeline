eca47f2dbf4f56bba090c1c0917e72af847632cb
Marisa DeMeglio <marisa.demeglio@gmail.com>
2011-06-27 10:42:19 -0700

    used IDFactory for job IDs.  still working on bundle integration.

---
+++ commandline/src/main/java/org/daisy/pipeline/ui/commandline/CmdLineActivator.java
@@ -0,0 +1,63 @@
+package org.daisy.pipeline.ui.commandline;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.daisy.pipeline.ui.commandline.provider.OSGIServiceProvider;
+import org.daisy.pipeline.ui.commandline.provider.ServiceProvider;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class CmdLineActivator implements BundleActivator {
+	private static boolean EXIT = true;
+	@Override
+	public void start(BundleContext context) throws Exception {
+		final BundleContext ctxt = context;
+		org.slf4j.impl.OSGILogFactory.initOSGI(context);
+		
+		new Thread() {
+			public void run() {
+			  //  for (Object key:System.getProperties().keySet()){
+			    //	System.out.println("[PROP] "+key+": " + System.getProperties().getProperty(key.toString()));
+			    //}
+				String args = null;
+				args= System.getProperty("org.daisy.pipeline.cmdargs");
+				//awful getevn thanks to the disappointing pax runner --vmo space support
+				if (args==null)
+					args = System.getenv("DAISY_ARGS");
+				//System.out.println("[ARGS] "+args);
+				ServiceProvider provider = new OSGIServiceProvider(ctxt);
+				if (args == null) {
+					new CommandLine(provider).getUnrecovreableError(
+							"The arguments are null").execute();
+					if(EXIT)
+						System.exit(1);
+				} else {
+
+					try {
+						new CommandLine(provider).parse(args.split("\\s"))
+								.execute();
+					} catch (Exception e) {
+						StringWriter sw = new StringWriter();
+						e.printStackTrace(new PrintWriter(sw));
+						new CommandLine(provider).getUnrecovreableError(e
+								.getMessage()+"\n"+sw.toString()).execute();
+						if(EXIT)
+							System.exit(1);
+					}
+					if(EXIT)
+						System.exit(0);
+				}
+
+			}
+		}.start();
+		
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ converter-registry/src/main/java/org/daisy/converter/registry/OSGIConverterRunner.java
@@ -0,0 +1,172 @@
+package org.daisy.converter.registry;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.URIResolver;
+import javax.xml.transform.stream.StreamResult;
+
+import net.sf.saxon.Configuration;
+
+import org.daisy.pipeline.modules.UriResolverDecorator;
+import org.daisy.pipeline.modules.converter.Converter.ConverterArgument.Type;
+import org.daisy.pipeline.modules.converter.ConverterDescriptor;
+import org.daisy.pipeline.modules.converter.ConverterRunnable;
+import org.daisy.pipeline.xproc.XProcessor;
+
+/**
+ * The Class OSGIConverterRunner runs the converter using a xproc wrapper instance 
+ * built using the xproc factory defined in the registry object 
+ */
+public class OSGIConverterRunner extends ConverterRunnable {
+
+	/** The  registry. */
+	OSGIConverterRegistry mRegistry;
+
+	/**
+	 * Instantiates a new oSGI converter runner.
+	 *
+	 * @param conv the converter
+	 */
+	protected OSGIConverterRunner(OSGIConverter conv) {
+		super(conv);
+		mRegistry = conv.getRegistry();
+		mExecutor = new OSGIConverterExecutor();
+	}
+
+	/**
+	 * The Class OSGIConverterExecutor, in charge of calling xproc wrapper and bind the arguments for a correct execution
+	 */
+	class OSGIConverterExecutor implements ConverterExecutor {
+
+		/* (non-Javadoc)
+		 * @see org.daisy.pipeline.modules.converter.ConverterRunnable.ConverterExecutor#execute(org.daisy.pipeline.modules.converter.ConverterRunnable)
+		 */
+		@Override
+		public void execute(ConverterRunnable runnable) {
+			ConverterDescriptor desc = mRegistry.getDescriptor(runnable
+					.getConverter().getName());
+			Source src;
+			try {
+				src = mRegistry.getUriResolver().resolve(
+						desc.getFile().toString(), "");
+			} catch (TransformerException e) {
+				throw new RuntimeException(
+						"Error while getting the converter file:"
+								+ e.getLocalizedMessage(), e);
+			}
+			XProcessor proc = mRegistry.getXprocFactory().getProcessor(src);
+			URIResolver defaultResolver = Configuration.newConfiguration().getURIResolver();
+			((UriResolverDecorator)mRegistry.getUriResolver()).setDelegatedUriResolver(defaultResolver);
+			proc.setURIResolver(mRegistry.getUriResolver());
+			bindInputs(proc, runnable);
+			bindOutputs(proc, runnable);
+			bindParams(proc, runnable);
+			bindOptions(proc, runnable);
+			// lets start the fun
+			proc.run();
+
+		}
+
+		/**
+		 * Bind inputs.
+		 *
+		 * @param proc the proc
+		 * @param runnable the runnable
+		 */
+		private void bindInputs(XProcessor proc, ConverterRunnable runnable) {
+			try {
+				for (ValuedConverterArgument arg : runnable.getValues()) {
+					if (arg.getArgument().getType() == Type.INPUT) {
+						Source src;
+
+						src = mRegistry.getUriResolver().resolve(
+								arg.getValue(), "");
+						proc.bindInputPort(arg.getArgument().getPort(), src);
+					}
+				}
+			} catch (TransformerException e) {
+				throw new RuntimeException("Error binding port:"
+						+ e.getLocalizedMessage(), e);
+			}
+
+		}
+
+		/**
+		 * Bind outputs.
+		 *
+		 * @param proc the proc
+		 * @param runnable the runnable
+		 */
+		private void bindOutputs(XProcessor proc, ConverterRunnable runnable) {
+
+			for (ValuedConverterArgument arg : runnable.getValues()) {
+				if (arg.getArgument().getType() == Type.OUTPUT) {
+
+					proc.bindOutputPort(arg.getArgument().getPort(),
+							getSaxResult(arg.getValue()));
+				}
+			}
+
+		}
+
+		/**
+		 * Bind params.
+		 *
+		 * @param proc the proc
+		 * @param runnable the runnable
+		 */
+		private void bindParams(XProcessor proc, ConverterRunnable runnable) {
+			for (ValuedConverterArgument arg : runnable.getValues()) {
+				if (arg.getArgument().getType() == Type.PARAMETER) {
+
+					proc.setParameter(arg.getArgument().getPort(), arg
+							.getArgument().getBind(), arg.getValue());
+				}
+			}
+		}
+
+		/**
+		 * Bind options.
+		 *
+		 * @param proc the proc
+		 * @param runnable the runnable
+		 */
+		private void bindOptions(XProcessor proc, ConverterRunnable runnable) {
+			for (ValuedConverterArgument arg : runnable.getValues()) {
+				if (arg.getArgument().getType() == Type.OPTION) {
+					proc.setOption(arg.getArgument().getBind(), arg.getValue());
+				}
+			}
+		}
+
+		/**
+		 * Gets the sax result.
+		 *
+		 * @param output the output
+		 * @return the sax result
+		 * @throws IllegalArgumentException the illegal argument exception
+		 */
+		private Result getSaxResult(String output)
+				throws IllegalArgumentException {
+			// return new StreamResult(System.out);
+			if (output == null || output.isEmpty()) {
+				return new StreamResult(System.out);
+			} else {
+
+				try {
+					return new StreamResult(new FileOutputStream(output));
+				} catch (FileNotFoundException e) {
+					throw new IllegalArgumentException("Output file not found:"
+							+ e);
+				}
+
+			}
+
+		}
+
+	}
+}
