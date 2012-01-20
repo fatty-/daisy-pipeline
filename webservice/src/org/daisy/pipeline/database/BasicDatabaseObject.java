54f89c24a512c2fc3cddcc7a7126cad8095527c0
Marisa DeMeglio <marisa.demeglio@gmail.com>
2012-01-20 13:55:57 -0800

    merged with default, added database backend

--- webservice/src/org/daisy/pipeline/database/BasicDatabaseObject.java
+++ webservice/src/org/daisy/pipeline/database/BasicDatabaseObject.java
@@ -1,5 +1,7 @@
 package org.daisy.pipeline.database;
 
-public class BasicDatabaseObject {
-
+// An abstract database object
+public abstract class BasicDatabaseObject {
+	public abstract String getInternalId();
+	public abstract void copyData(BasicDatabaseObject object);		
 }
