6021d04fb195b72966e9a6a4eef5e8fc1872e956
capitan cambio <capitan.cambio@gmail.com>
2012-02-08 15:24:30 +0000

    io problems solved xplatform

---
+++ CLI/core/helpers.rb
@@ -0,0 +1,28 @@
+require_rel './core/ctxt'
+module Helpers 
+
+  	module_function
+	def normalise_name(str) 
+		if str != nil
+			str=str.downcase
+			str=str.gsub(/\s/,'-')
+		else
+			Ctxt.logger.warning("name is nil")
+		end
+		return str
+	end
+		
+	def path_to_uri(path,is_local)
+		Ctxt.logger.debug("transforming path uri")
+		if is_local	
+			uri=File.expand_path(path).gsub(/\\/,'/')
+			uri='/'+uri if uri[0]!='/'[0]
+			uri="file:"+uri
+					
+		else
+			uri=path
+		end
+		Ctxt.logger.debug("uri: #{uri}")
+		return URI.escape(uri)
+	end
+end
